bb008678728a5bd25f5236d0b5c76e2c363b0963
Ivy930626 <chiivy0626@gmail.com>
2024-05-14 22:48:22 +0800

    第三次修改

--- app/src/main/java/tw/edu/pu/csim/cindi/finalproject2/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/cindi/finalproject2/MainActivity.kt
@@ -1,14 +1,22 @@
 package tw.edu.pu.csim.cindi.finalproject2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.cindi.finalproject2.ui.theme.Finalproject2Theme
 
@@ -22,7 +30,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    FirstScreen()
                 }
             }
         }
@@ -30,17 +39,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello123 $name!",
-        modifier = modifier
+fun FirstScreen(){
+    val context = LocalContext.current
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Finalproject2Theme {
-        Greeting("Android")
+    {
+        Button(onClick = {
+            var it = Intent(context, SecondActivity::class.java)
+            context.startActivity(it)
+        })
+        {
+            Text(text = "顏色測驗")
+        }
     }
 }
