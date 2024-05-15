f3e30e9a00fd0c68fc03c95f014a168cfd3e8894
Jenny411146928 <jenny1211jenny@gmail.com>
2024-05-15 22:29:47 +0800

    選單圖式+標題+作者圖式

--- app/src/main/java/tw/edu/pu/csim/cindi/finalproject2/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/cindi/finalproject2/MainActivity.kt
@@ -2,16 +2,24 @@ package tw.edu.pu.csim.cindi.finalproject2
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.rounded.AccountBox
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,6 +40,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     //Greeting("Android")
                     FirstScreen()
+                    Main()
                 }
             }
         }
@@ -56,5 +65,35 @@ fun FirstScreen() {
         {
             Text(text = "顏色測驗")
         }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Main() {
+    val context = LocalContext.current
+
+    Column {
+        TopAppBar(
+            title = { Text(text = "APP名") },
+
+            navigationIcon = {
+                IconButton(onClick = {
+                    Toast.makeText(context, "您點選了導覽圖示", Toast.LENGTH_SHORT).show()
+                }) {
+                    Icon(Icons.Default.Menu, contentDescription = "Navigation icon")
+                }
+            },
+            actions = {
+                IconButton(
+                    onClick = { Toast.makeText(context, "作者：米納桑", Toast.LENGTH_SHORT).show() }
+                ) {
+                    Icon(Icons.Rounded.AccountBox, contentDescription = "Author")
+                }
+            }
+
+
+        )
     }
 }
