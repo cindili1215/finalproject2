12b6e91d043d41239ad2cf3e1e17531da7d3a57f
hsiien53 <hsien03593@gmail.com>
2024-05-18 15:09:34 +0800

    start!

--- app/src/main/java/tw/edu/pu/csim/cindi/finalproject2/SecondActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/cindi/finalproject2/SecondActivity.kt
@@ -1,17 +1,21 @@
 package tw.edu.pu.csim.cindi.finalproject2
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.cindi.finalproject2.ui.theme.Finalproject2Theme
+import android.widget.Button as Button1
 
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,3 +42,4 @@ fun Greeting() {
         )
     }
 }
+
